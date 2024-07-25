6034504bacc6b471a23049fa9f3cfe1308c66b4b
RoniaZhang <XiaQian1999@gmail.com>
2024-07-25 17:16:27 -0400

    Food Delivery App - Sign In/Sign Up Pop Up modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Navbar from './components/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
@@ -6,18 +6,23 @@ import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 
 const App = props => {
+
+  const [showLogin, setShowLogin] = useState(false);
+  
   return (
     <>
+    {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
         </Routes>
-      </div>
+      </div>  
 
       <Footer />
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("home");
     return (
@@ -23,7 +23,7 @@ const Navbar = () => {
                     <div className="dot"></div>
                 </div>
 
-                <button>sign in</button>
+                <button onClick={() => setShowLogin(true)}>sign in</button>
             </div>
         </div>
     )
