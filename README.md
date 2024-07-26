0253764e85f49a48dfc05e9c72cee26c1965d1bb
RoniaZhang <XiaQian1999@gmail.com>
2024-07-25 21:25:16 -0400

    Food Delivery App - Cart Page frontend mainly done

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
 # Food Delivery Web Page
 
-### Tool in Use
+## Tool in Use
 
-### Initial Set Up for dependencies
+## Features include inside this app
+
+### Frontend
+
+- User able to increment the count for each menu item from Home page
+- User able to decrement the count for each menu item from Home page
+- The increment of the count will reflect in the cart page
+- User able to remove the menu item from the Cart Page
+- User can filter the menu item based on the Food Category
+- Responsive Design with mobile friendly
+
+### Backend
+
+## Initial Set Up for dependencies
 
 - npm install
 - npm install react-router-dom
 
-### New Things Learn
+## New Things Learn
 
 - Set Up React Router
 
@@ -25,7 +38,7 @@
     <Route path='/order' element={<PlaceOrder />} />
    </Routes> -->
 
-### Question need for reasearch
+## Question need for reasearch
 
 1. what is @keyframe in the css
    - It could reused in the transition with opacity with some UI/UX design
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = ({setShowLogin}) => {
     const [menu, setMenu] = useState("home");
     return (
         <div className='navbar'>
-            <img src={assets.logo} alt="" className="logo" />
+            <Link to="/"><img src={assets.logo} alt="" className="logo" /></Link>
             <ul className="navbar-menu">
                 <Link to='/' onClick={()=>setMenu("home")} className={menu==="home" ? "active" : ""}>home</Link>
                 <a href="#explore-menu" onClick={()=>setMenu("menu")} className={menu==="menu" ? "active" : ""}>menu</a>
@@ -19,7 +19,9 @@ const Navbar = ({setShowLogin}) => {
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="search icon" />
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt="" />
+                    <Link to="/cart">
+                        <img src={assets.basket_icon} alt="" />
+                    </Link>
                     <div className="dot"></div>
                 </div>
 
