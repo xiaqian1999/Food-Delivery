8be20d70b86e0e7483338244125fb7ecfd777dcb
RoniaZhang <XiaQian1999@gmail.com>
2024-07-26 18:43:51 -0400

    Food Delivery App - backend in separate folder

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-//connect with DB
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatstack:1234567greatstack@cluster0.0oahry3.mongodb.net/food-del').then(() => console.log("DB Connected"));
-}
