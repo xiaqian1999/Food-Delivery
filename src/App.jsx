455394e28a2ed82f3e459a0f63631ebcdf2ce86b
RoniaZhang <XiaQian1999@gmail.com>
2024-07-29 16:44:52 -0400

    Food Delivery Backend - setup with verify page after payment info submit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
 import LoginPopup from './components/LoginPopup/LoginPopup';
+import Verify from './pages/Verify/Verify';
 
 const App = props => {
 
@@ -21,6 +22,7 @@ const App = props => {
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/verify' element={<Verify />} />
         </Routes>
       </div>  
 
