aaf57cc0d5ade888b0a18a37487e77a92096423c
RoniaZhang <XiaQian1999@gmail.com>
2024-07-29 17:08:29 -0400

    Food Delivery Backend - redirect to correct page after payment verify success

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Verify.css';
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 
 const Verify = () => {
@@ -9,6 +10,20 @@ const Verify = () => {
     const success = searchParams.get("success");
     const orderId = searchParams.get("orderId");
     const {url} = useContext(StoreContext);
+    const navigate = useNavigate();
+
+    const verifyPayment = async () => {
+        const response = await axios.post(url+"/api/order/verify", {success, orderId});
+        if (response.data.success) {
+            navigate("/myorders");
+        }else{
+            navigate("/");
+        }
+    }
+
+    useEffect(() => {
+        verifyPayment();
+    }, [])
 
     return (
         <div className='verify'> 
